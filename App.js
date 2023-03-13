79aee6a3ba7fb2140d77ba29a11a7a9e59110af7
Scott Foster <scott@imfoster.com>
2023-03-13 13:19:32 -0500

    major modifications

--- App.js
+++ App.js
@@ -4,10 +4,7 @@ import { useState } from "react";
 import {
   View,
   FlatList,
-  StyleSheet,
   Text,
-  StatusBar,
-  Button,
   Dimensions,
   TouchableOpacity,
   Image,
@@ -22,51 +19,81 @@ import Moment from "react-moment";
 const screenWidth = Dimensions.get("window").width;
 
 function HomeScreen({ navigation }) {
-
-  const [profilePicSelected, setProfilePicSelected] = useState('0');
+  const [mainImageSelected, setMainImageSelected] = useState("0");
 
   const onViewableItemsChanged = (info) => {
-    setProfilePicSelected(info.viewableItems[0].index)
+    setMainImageSelected(info.viewableItems[0].index);
   };
-  
+
   const viewabilityConfigCallbackPairs = React.useRef([
     { onViewableItemsChanged },
   ]);
-  
 
-  const DATA = [
+  const SUNRISES = [
     {
       id: "1",
-      location: "Dallas, TX",
-      image: "https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg",
-      time: "2023-03-10T10:59-0500",
-      author: "@scottfoster",
+      timezone: "CST",
+      images: [
+        {
+          id: "1",
+          location: "Dallas, TX",
+          image:
+            "https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg",
+          time: "2023-03-10T10:59-0500",
+          author: "@scottfoster",
+        },
+        {
+          id: "2",
+          location: "Plano, TX",
+          image:
+            "https://images.pexels.com/photos/36744/agriculture-arable-clouds-countryside.jpg",
+          time: "2023-03-11T10:59-0500",
+          author: "@bobsmith",
+        },
+        {
+          id: "3",
+          location: "Chicago, IL",
+          image:
+            "https://images.pexels.com/photos/1323550/pexels-photo-1323550.jpeg",
+          time: "2023-03-12T10:59-0500",
+          author: "@theblah",
+        },
+      ],
     },
     {
       id: "2",
-      location: "Plano, TX",
-      image:
-        "https://images.pexels.com/photos/36744/agriculture-arable-clouds-countryside.jpg",
-      time: "2023-03-11T10:59-0500",
-      author: "@scottfoster",
-    },
-    {
-      id: "3",
-      location: "Chicago, IL",
-      image:
-        "https://images.pexels.com/photos/1323550/pexels-photo-1323550.jpeg",
-      time: "2023-03-12T10:59-0500",
-      author: "@scottfoster",
+      timezone: "MST",
+      images: [
+        {
+          id: "1",
+          location: "Not Dallas, TX",
+          image:
+            "https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg",
+          time: "2023-03-10T10:59-0500",
+          author: "@scottfoster",
+        },
+        {
+          id: "2",
+          location: "Plano, TX",
+          image:
+            "https://images.pexels.com/photos/36744/agriculture-arable-clouds-countryside.jpg",
+          time: "2023-03-11T10:59-0500",
+          author: "@bobsmith",
+        },
+      ],
     },
   ];
 
-  const Item = ({ item }) => (
+  const HeaderItem = ({ item }) => (
     <TouchableOpacity
       activeOpacity={1}
       onPress={() => {
+        console.log("next");
+        /*
         navigation.navigate("Details", {
           itemId: 100,
         });
+        */
       }}
     >
       <View style={{ width: screenWidth - 40 }}>
@@ -79,74 +106,88 @@ function HomeScreen({ navigation }) {
             width: "100%",
           }}
         />
-
-        <View tw="absolute mt-5 color-white text-center w-full">
-          <Text tw="color-white text-center">
-            {item.id}/{DATA.length}
-          </Text>
-        </View>
-
-        <View tw="flex flex-row justify-between pt-2">
-          <Text tw="font-semibold">{item.location}</Text>
-          <Moment fromNow element={Text}>
-            {item.time}
-          </Moment>
-        </View>
-        <View>
-          <Text tw="italic">{item.author}</Text>
-        </View>
       </View>
     </TouchableOpacity>
+  );
+
+  const BodyItem = ({ item }) => (
+    <View tw="flex flex-row justify-between pt-2">
+      <View>
+        <Text>{item.timezone}</Text>
+      </View>
+      <View>
+        <FlatList
+          horizontal
+          data={item.images}
+          renderItem={({ item }) => <BodyItemImage item={item} />}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
+    </View>
+  );
+
+  const BodyItemImage = ({ item }) => (
+    <Image
+      tw="h-24 w-24 rounded-xl"
+      source={{
+        uri: item.image,
+      }}
+    />
   );
 
   return (
     <View tw="bg-slate-300 h-screen">
       <SafeAreaView tw="px-5">
-        <Text tw="text-2xl font-bold">Sunrises for GMT-6</Text>
-        <Text>
+        <Text tw="text-2xl font-bold py-0.5">
+          Current Sunrises for {SUNRISES[0].timezone}
+        </Text>
+        <Text tw="pb-2">
           Current Time: <Moment local element={Text} />
         </Text>
 
         <FlatList
-          data={DATA}
-          renderItem={({ item, index }) => <Item item={item} />}
+          data={SUNRISES[0].images}
+          renderItem={({ item }) => <HeaderItem item={item} />}
           horizontal
           pagingEnabled
           keyExtractor={(item) => item.id}
           showsHorizontalScrollIndicator={false}
           initialNumToRender={0}
           decelerationRate={"fast"}
-          // onViewableItemsChanged={onViewableItemsChanged}
-          viewabilityConfig={{waitForInteraction: true, viewAreaCoveragePercentThreshold: 95}}
-          viewabilityConfigCallbackPairs={viewabilityConfigCallbackPairs.current}
-        
+          viewabilityConfig={{
+            waitForInteraction: true,
+            viewAreaCoveragePercentThreshold: 95,
+          }}
+          viewabilityConfigCallbackPairs={
+            viewabilityConfigCallbackPairs.current
+          }
         />
 
-        <Text>abz - {profilePicSelected}</Text>
+        <View tw="flex flex-row justify-between pt-2">
+          <Text tw="font-semibold">
+            {SUNRISES[0].images[mainImageSelected].location}
+          </Text>
+          <Moment fromNow element={Text}>
+            {SUNRISES[0].images[mainImageSelected].time}
+          </Moment>
+        </View>
+        <View>
+          <Text tw="italic">
+            {SUNRISES[0].images[mainImageSelected].author}
+          </Text>
+        </View>
+
+        <Text tw="text-2xl font-bold py-0.5">Sunrises Around the World</Text>
+
+        <FlatList
+          data={SUNRISES}
+          renderItem={({ item }) => <BodyItem item={item} />}
+          keyExtractor={(item) => item.id}
+        />
       </SafeAreaView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-  },
-  item: {
-    backgroundColor: "#f9c2ff",
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },
-  title: {
-    fontSize: 32,
-  },
-  image: {
-    width: "100%",
-    height: 300,
-  },
-});
 
 const Stack = createNativeStackNavigator();
 
@@ -155,11 +196,10 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator
         screenOptions={{
-          headerShown: false,
           animation: "none",
         }}
       >
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="24sunrises" component={HomeScreen} />
         <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
